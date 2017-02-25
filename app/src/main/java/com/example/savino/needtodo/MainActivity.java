5fd5eccd91cd266d8a2f0f232a62646ba0b6f8f4
Savino Ordine <savino.ordine@scmitaly.it>
2017-02-25 15:06:37 +0100

    data binding

--- app/src/main/java/com/example/savino/needtodo/MainActivity.java
+++ app/src/main/java/com/example/savino/needtodo/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.savino.needtodo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.savino.needtodo.databinding.ActivityMainBinding;
+import com.example.savino.needtodo.model.Person;
 
 public class MainActivity extends AppCompatActivity {
+
+    // The name is the camel case of same of the layout inflated + Binding
+    // activity_main --> ActivityMainBinding
+    private ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        Person person = new Person("Savino", "Ordine", 32);
+
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        mBinding.setPerson(person);
     }
 }
---
+++ app/src/main/java/com/example/savino/needtodo/model/Person.java
@@ -0,0 +1,47 @@
+package com.example.savino.needtodo.model;
+
+public class Person {
+
+    String name;
+    String surname;
+    int age;
+
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person)) return false;
+
+        Person person = (Person) o;
+
+        if (age != person.age) return false;
+        if (name != null ? !name.equals(person.name) : person.name != null) return false;
+        return surname != null ? surname.equals(person.surname) : person.surname == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (surname != null ? surname.hashCode() : 0);
+        result = 31 * result + age;
+        return result;
+    }
+}
